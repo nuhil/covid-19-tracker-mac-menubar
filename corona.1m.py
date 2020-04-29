c1357ef7b6e2abc46497fa70caafc67fb1e17e1a
Nuhil Mehdy <1129785+nuhil@users.noreply.github.com>
2020-04-29 13:44:53 -0600

    Add files via upload

---
+++ corona.1m.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# <bitbar.title>COVID-19 Tracker</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Nuhil Mehdy</bitbar.author>
+# <bitbar.author.github>nuhil</bitbar.author.github>
+# <bitbar.desc>Watch COVID-19 Statistics</bitbar.desc>
+# <bitbar.image>https://github.com/nuhil/covid-19-tracker-mac-menubar/blob/master/icon.gif?raw=true</bitbar.image>
+# <bitbar.dependencies>python</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/nuhil/covid-19-tracker-mac-menubar</bitbar.abouturl>
+
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://covid19.mathdro.id/api/countries/USA")
+data = response.json()
+print("😷" + str(data['confirmed']['value']) + "😀" + str(data['recovered']['value']) + "💀" + str(data['deaths']['value']))
